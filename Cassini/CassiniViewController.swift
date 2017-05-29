9ab21f4e1b74a5bb8f9168c88550f762829c52ae
Hartrickb <Hartrickb@users.noreply.github.com>
2017-05-29 09:18:54 -0400

    feat: added ability to work on iPad and iPhone

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Bennett Hartrick on 5/29/17.
+//  Copyright © 2017 Bennett. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
